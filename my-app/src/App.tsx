74f738a0afe8f60b642d31fa34ab6f00df8eac0d
Mickel-Barros <mickelbarros@yahoo.com>
2023-04-18 12:12:49 -0300

    feat: add user page

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,4 +1,5 @@
 import HomePage from './pages/HomePage';
+import UserPage from './pages/UserPage';
 import 'react-toastify/dist/ReactToastify.css';
 import { Routes, Route } from 'react-router-dom';
 import GlobalStyle from './styles/global';
@@ -8,7 +9,7 @@ function App() {
   return (
     <>
       <GlobalStyle/>
-      <HomePage/>
+      <UserPage/>
     </>
   );
 }
