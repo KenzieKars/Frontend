3b1ee259bd997e363ae88be7934ce434f2f3104f
Mickel-Barros <mickelbarros@yahoo.com>
2023-04-20 11:16:31 -0300

    fix: merge conflicts

--- my-app/src/pages/UserPage/index.tsx
+++ my-app/src/pages/UserPage/index.tsx
@@ -11,26 +11,72 @@ import MobileMenu from "../../components/mobileMenu";
 import { api } from "../../services/api";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { NewAdModal } from "../../components/modal";
 
 const banner = require('../../assets/car.png') as string;
 const logo = require('../../assets/logo.png') as string;
 
+interface IUserInfo {
+    id: string  
+    nome: string
+    email: string              
+    telefone:string
+    bio:string
+    imagem:string
+    cpf:string
+    aniversario:string
+    vendedor?: boolean;
+}
 
 function UserPage() {
 	const [newAdModal, setNewAdModal] = useState(false);
+    const [userInfo, setUserInfo] = useState<IUserInfo>({} as IUserInfo);
+    const [anunciosInfo, setAnuncioInfo] = useState([]);
+    const token: string | null = localStorage.getItem("@user:Token");
+    const userId: string | null = localStorage.getItem("@user:ID");
+    const navigate = useNavigate()
+    if(!token){
+        navigate("/")
+    }
+    useEffect(() => {
+        token &&
+          api.get(`/users/${userId}`, {
+            headers: { Authorization: `Bearer ${token}` },
+          })
+            .then((res) => {
+              setUserInfo(res.data.foundUserByParam);
+            })
+            .catch((err) => {
+              window.localStorage.clear();
+              navigate("/login");
+            });
+      }, []);
 
+      useEffect(() => {
+        token &&
+          api.get(`/advertisement/users/${userId}`, {
+            headers: { Authorization: `Bearer ${token}` },
+          })
+            .then((res) => {
+                setAnuncioInfo(res.data);
+            })
+            .catch((err) => {
+              window.localStorage.clear();
+              navigate("/login");
+            });
+      }, []);
 	return (
 		<Div>
 			<Nav>
 				<figure>
-					<img src={logo} alt="logo da empresa" />
+					<img src={logo} alt="logo da empresa" onClick={()=>{navigate("/")}}/>
 				</figure>
 				<DivNavBarUser>
 					<img
 						src="https://cdn-icons-png.flaticon.com/512/21/21104.png"
 						alt="user"
 					/>
-					<p>Nome usuário</p>
+					<p>{userInfo.nome}</p>
 				</DivNavBarUser>
 			</Nav>
 			<MobileMenu></MobileMenu>
@@ -46,15 +92,15 @@ function UserPage() {
 						className="product-title"
 						titleSize="Heading-6-600"
 					>
-						Nome usuário
+						{userInfo.nome}
 					</ThemeTitle>
-					<span>Anunciante</span>
+					<span>
+                    {userInfo.vendedor? "Anunciante" : "Comprador"}
+          
+                    </span>
 				</div>
 				<p>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-					Iusto quis neque optio, ea mollitia non id voluptates eum
-					consequatur, nulla, numquam voluptas accusantium consectetur
-					similique iste adipisci maxime? Debitis, id.
+                    {userInfo.bio}
 				</p>
 				<Button
 					backgroundColor="#FDFDFD"
@@ -77,11 +123,14 @@ function UserPage() {
 			</ThemeTitle>
 			<Main>
 				<Products>
-					<ProductContainer>
+                    {anunciosInfo.map((anuncio)=>{
+                        console.log(anuncio)
+                        return(
+                            <ProductContainer>
 						<div>
 							<img
 								className="product-img"
-								src="https://bluesky-cogcms-prodb.cdn.imgeng.in/media/5o4jo4yv/quattroporte.png?width=520"
+								src={anuncio.imagem? anuncio.imagem :"https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-1-scaled.png"}
 								alt="anuncio"
 							/>
 						</div>
@@ -90,13 +139,11 @@ function UserPage() {
 							className="product-title"
 							titleSize="Heading-7-600"
 						>
-							Miserati - Ghibli
+							{anuncio.marca} - {anuncio.modelo}
 						</ThemeTitle>
 						<div className="product-description">
 							<p>
-								Lorem ipsum dolor sit amet consectetur
-								adipisicing elit. Quam facere optio unde aliquid
-								error dolorem...
+                                {anuncio.descricao}
 							</p>
 						</div>
 
@@ -106,15 +153,20 @@ function UserPage() {
 								src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"
 								alt="user"
 							/>
-							<span className="owner-name">Username</span>
+							<span className="owner-name">
+                            {anuncio.user.nome}
+                                
+                            </span>
 						</ProductOwner>
 
 						<ProductDetails>
 							<div className="product-details">
-								<p className="product-mileage">0 KM</p>
-								<p className="product-year">2019</p>
+								<p className="product-mileage">
+                                {anuncio.quilometragem} KM</p>
+								<p className="product-year">
+                                {anuncio.ano}</p>
 							</div>
-							<span className="product-price">R$ 12.000,00</span>
+							<span className="product-price">R$ {anuncio.preco}</span>
 						</ProductDetails>
 						<div>
 							<Button
@@ -145,482 +197,10 @@ function UserPage() {
 							</Button>
 						</div>
 					</ProductContainer>
-					<ProductContainer>
-						<div>
-							<img
-								className="product-img"
-								src="https://bluesky-cogcms-prodb.cdn.imgeng.in/media/5o4jo4yv/quattroporte.png?width=520"
-								alt="anuncio"
-							/>
-						</div>
-						<ThemeTitle
-							tag="h2"
-							className="product-title"
-							titleSize="Heading-7-600"
-						>
-							Miserati - Ghibli
-						</ThemeTitle>
-						<div className="product-description">
-							<p>
-								Lorem ipsum dolor sit amet consectetur
-								adipisicing elit. Quam facere optio unde aliquid
-								error dolorem...
-							</p>
-						</div>
+                        )
+                    })}
+					
 
-						<ProductOwner>
-							<img
-								className="owner-avatar"
-								src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"
-								alt="user"
-							/>
-							<span className="owner-name">Username</span>
-						</ProductOwner>
-
-						<ProductDetails>
-							<div className="product-details">
-								<p className="product-mileage">0 KM</p>
-								<p className="product-year">2019</p>
-							</div>
-							<span className="product-price">R$ 12.000,00</span>
-						</ProductDetails>
-						<div>
-							<Button
-								backgroundColor="#FDFDFD"
-								border="1.5px solid #212529"
-								backgroundColorHover=""
-								borderHover=""
-								fontColor="#212529"
-								fontColorHover=""
-								onClick={() => {}}
-								type="button"
-								className="editarAnuncios"
-							>
-								Editar
-							</Button>
-							<Button
-								backgroundColor="#FDFDFD"
-								border="1.5px solid #212529"
-								backgroundColorHover=""
-								borderHover=""
-								fontColor="#212529"
-								fontColorHover=""
-								onClick={() => {}}
-								type="button"
-								className="verAnuncios"
-							>
-								Ver detalhes
-							</Button>
-						</div>
-					</ProductContainer>
-					<ProductContainer>
-						<div>
-							<img
-								className="product-img"
-								src="https://bluesky-cogcms-prodb.cdn.imgeng.in/media/5o4jo4yv/quattroporte.png?width=520"
-								alt="anuncio"
-							/>
-						</div>
-						<ThemeTitle
-							tag="h2"
-							className="product-title"
-							titleSize="Heading-7-600"
-						>
-							Miserati - Ghibli
-						</ThemeTitle>
-						<div className="product-description">
-							<p>
-								Lorem ipsum dolor sit amet consectetur
-								adipisicing elit. Quam facere optio unde aliquid
-								error dolorem...
-							</p>
-						</div>
-
-						<ProductOwner>
-							<img
-								className="owner-avatar"
-								src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"
-								alt="user"
-							/>
-							<span className="owner-name">Username</span>
-						</ProductOwner>
-
-						<ProductDetails>
-							<div className="product-details">
-								<p className="product-mileage">0 KM</p>
-								<p className="product-year">2019</p>
-							</div>
-							<span className="product-price">R$ 12.000,00</span>
-						</ProductDetails>
-						<div>
-							<Button
-								backgroundColor="#FDFDFD"
-								border="1.5px solid #212529"
-								backgroundColorHover=""
-								borderHover=""
-								fontColor="#212529"
-								fontColorHover=""
-								onClick={() => {}}
-								type="button"
-								className="editarAnuncios"
-							>
-								Editar
-							</Button>
-							<Button
-								backgroundColor="#FDFDFD"
-								border="1.5px solid #212529"
-								backgroundColorHover=""
-								borderHover=""
-								fontColor="#212529"
-								fontColorHover=""
-								onClick={() => {}}
-								type="button"
-								className="verAnuncios"
-							>
-								Ver detalhes
-							</Button>
-						</div>
-					</ProductContainer>
-					<ProductContainer>
-						<div>
-							<img
-								className="product-img"
-								src="https://bluesky-cogcms-prodb.cdn.imgeng.in/media/5o4jo4yv/quattroporte.png?width=520"
-								alt="anuncio"
-							/>
-						</div>
-						<ThemeTitle
-							tag="h2"
-							className="product-title"
-							titleSize="Heading-7-600"
-						>
-							Miserati - Ghibli
-						</ThemeTitle>
-						<div className="product-description">
-							<p>
-								Lorem ipsum dolor sit amet consectetur
-								adipisicing elit. Quam facere optio unde aliquid
-								error dolorem...
-							</p>
-						</div>
-
-						<ProductOwner>
-							<img
-								className="owner-avatar"
-								src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"
-								alt="user"
-							/>
-							<span className="owner-name">Username</span>
-						</ProductOwner>
-
-						<ProductDetails>
-							<div className="product-details">
-								<p className="product-mileage">0 KM</p>
-								<p className="product-year">2019</p>
-							</div>
-							<span className="product-price">R$ 12.000,00</span>
-						</ProductDetails>
-						<div>
-							<Button
-								backgroundColor="#FDFDFD"
-								border="1.5px solid #212529"
-								backgroundColorHover=""
-								borderHover=""
-								fontColor="#212529"
-								fontColorHover=""
-								onClick={() => {}}
-								type="button"
-								className="editarAnuncios"
-							>
-								Editar
-							</Button>
-							<Button
-								backgroundColor="#FDFDFD"
-								border="1.5px solid #212529"
-								backgroundColorHover=""
-								borderHover=""
-								fontColor="#212529"
-								fontColorHover=""
-								onClick={() => {}}
-								type="button"
-								className="verAnuncios"
-							>
-								Ver detalhes
-							</Button>
-						</div>
-					</ProductContainer>
-					<ProductContainer>
-						<div>
-							<img
-								className="product-img"
-								src="https://bluesky-cogcms-prodb.cdn.imgeng.in/media/5o4jo4yv/quattroporte.png?width=520"
-								alt="anuncio"
-							/>
-						</div>
-						<ThemeTitle
-							tag="h2"
-							className="product-title"
-							titleSize="Heading-7-600"
-						>
-							Miserati - Ghibli
-						</ThemeTitle>
-						<div className="product-description">
-							<p>
-								Lorem ipsum dolor sit amet consectetur
-								adipisicing elit. Quam facere optio unde aliquid
-								error dolorem...
-							</p>
-						</div>
-
-						<ProductOwner>
-							<img
-								className="owner-avatar"
-								src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"
-								alt="user"
-							/>
-							<span className="owner-name">Username</span>
-						</ProductOwner>
-
-						<ProductDetails>
-							<div className="product-details">
-								<p className="product-mileage">0 KM</p>
-								<p className="product-year">2019</p>
-							</div>
-							<span className="product-price">R$ 12.000,00</span>
-						</ProductDetails>
-						<div>
-							<Button
-								backgroundColor="#FDFDFD"
-								border="1.5px solid #212529"
-								backgroundColorHover=""
-								borderHover=""
-								fontColor="#212529"
-								fontColorHover=""
-								onClick={() => {}}
-								type="button"
-								className="editarAnuncios"
-							>
-								Editar
-							</Button>
-							<Button
-								backgroundColor="#FDFDFD"
-								border="1.5px solid #212529"
-								backgroundColorHover=""
-								borderHover=""
-								fontColor="#212529"
-								fontColorHover=""
-								onClick={() => {}}
-								type="button"
-								className="verAnuncios"
-							>
-								Ver detalhes
-							</Button>
-						</div>
-					</ProductContainer>
-					<ProductContainer>
-						<div>
-							<img
-								className="product-img"
-								src="https://bluesky-cogcms-prodb.cdn.imgeng.in/media/5o4jo4yv/quattroporte.png?width=520"
-								alt="anuncio"
-							/>
-						</div>
-						<ThemeTitle
-							tag="h2"
-							className="product-title"
-							titleSize="Heading-7-600"
-						>
-							Miserati - Ghibli
-						</ThemeTitle>
-						<div className="product-description">
-							<p>
-								Lorem ipsum dolor sit amet consectetur
-								adipisicing elit. Quam facere optio unde aliquid
-								error dolorem...
-							</p>
-						</div>
-
-						<ProductOwner>
-							<img
-								className="owner-avatar"
-								src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"
-								alt="user"
-							/>
-							<span className="owner-name">Username</span>
-						</ProductOwner>
-
-						<ProductDetails>
-							<div className="product-details">
-								<p className="product-mileage">0 KM</p>
-								<p className="product-year">2019</p>
-							</div>
-							<span className="product-price">R$ 12.000,00</span>
-						</ProductDetails>
-						<div>
-							<Button
-								backgroundColor="#FDFDFD"
-								border="1.5px solid #212529"
-								backgroundColorHover=""
-								borderHover=""
-								fontColor="#212529"
-								fontColorHover=""
-								onClick={() => {}}
-								type="button"
-								className="editarAnuncios"
-							>
-								Editar
-							</Button>
-							<Button
-								backgroundColor="#FDFDFD"
-								border="1.5px solid #212529"
-								backgroundColorHover=""
-								borderHover=""
-								fontColor="#212529"
-								fontColorHover=""
-								onClick={() => {}}
-								type="button"
-								className="verAnuncios"
-							>
-								Ver detalhes
-							</Button>
-						</div>
-					</ProductContainer>
-					<ProductContainer>
-						<div>
-							<img
-								className="product-img"
-								src="https://bluesky-cogcms-prodb.cdn.imgeng.in/media/5o4jo4yv/quattroporte.png?width=520"
-								alt="anuncio"
-							/>
-						</div>
-						<ThemeTitle
-							tag="h2"
-							className="product-title"
-							titleSize="Heading-7-600"
-						>
-							Miserati - Ghibli
-						</ThemeTitle>
-						<div className="product-description">
-							<p>
-								Lorem ipsum dolor sit amet consectetur
-								adipisicing elit. Quam facere optio unde aliquid
-								error dolorem...
-							</p>
-						</div>
-
-						<ProductOwner>
-							<img
-								className="owner-avatar"
-								src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"
-								alt="user"
-							/>
-							<span className="owner-name">Username</span>
-						</ProductOwner>
-
-						<ProductDetails>
-							<div className="product-details">
-								<p className="product-mileage">0 KM</p>
-								<p className="product-year">2019</p>
-							</div>
-							<span className="product-price">R$ 12.000,00</span>
-						</ProductDetails>
-						<div>
-							<Button
-								backgroundColor="#FDFDFD"
-								border="1.5px solid #212529"
-								backgroundColorHover=""
-								borderHover=""
-								fontColor="#212529"
-								fontColorHover=""
-								onClick={() => {}}
-								type="button"
-								className="editarAnuncios"
-							>
-								Editar
-							</Button>
-							<Button
-								backgroundColor="#FDFDFD"
-								border="1.5px solid #212529"
-								backgroundColorHover=""
-								borderHover=""
-								fontColor="#212529"
-								fontColorHover=""
-								onClick={() => {}}
-								type="button"
-								className="verAnuncios"
-							>
-								Ver detalhes
-							</Button>
-						</div>
-					</ProductContainer>
-					<ProductContainer>
-						<div>
-							<img
-								className="product-img"
-								src="https://bluesky-cogcms-prodb.cdn.imgeng.in/media/5o4jo4yv/quattroporte.png?width=520"
-								alt="anuncio"
-							/>
-						</div>
-						<ThemeTitle
-							tag="h2"
-							className="product-title"
-							titleSize="Heading-7-600"
-						>
-							Miserati - Ghibli
-						</ThemeTitle>
-						<div className="product-description">
-							<p>
-								Lorem ipsum dolor sit amet consectetur
-								adipisicing elit. Quam facere optio unde aliquid
-								error dolorem...
-							</p>
-						</div>
-
-						<ProductOwner>
-							<img
-								className="owner-avatar"
-								src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"
-								alt="user"
-							/>
-							<span className="owner-name">Username</span>
-						</ProductOwner>
-
-						<ProductDetails>
-							<div className="product-details">
-								<p className="product-mileage">0 KM</p>
-								<p className="product-year">2019</p>
-							</div>
-							<span className="product-price">R$ 12.000,00</span>
-						</ProductDetails>
-						<div>
-							<Button
-								backgroundColor="#FDFDFD"
-								border="1.5px solid #212529"
-								backgroundColorHover=""
-								borderHover=""
-								fontColor="#212529"
-								fontColorHover=""
-								onClick={() => {}}
-								type="button"
-								className="editarAnuncios"
-							>
-								Editar
-							</Button>
-							<Button
-								backgroundColor="#FDFDFD"
-								border="1.5px solid #212529"
-								backgroundColorHover=""
-								borderHover=""
-								fontColor="#212529"
-								fontColorHover=""
-								onClick={() => {}}
-								type="button"
-								className="verAnuncios"
-							>
-								Ver detalhes
-							</Button>
-						</div>
-					</ProductContainer>
 				</Products>
 			</Main>
 
@@ -635,6 +215,6 @@ function UserPage() {
 
 			{newAdModal && <NewAdModal setNewAdModal={setNewAdModal} />}
 		</Div>
-	);
+	);}
 
 export default UserPage;
